a44c8bff099352abc66968970422fa34467aaf88
jeferson <jefersonqueiroga@gmail.com>
2019-03-01 20:14:21 -0300

    Alteacao 01

--- AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
+++ AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
@@ -4,4 +4,11 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Testando git");
 	}
+		
+	public static void testeImpresao() {
+		System.out.println("teste fdfdfdf");
+	}
+	
+	
+	
 }
