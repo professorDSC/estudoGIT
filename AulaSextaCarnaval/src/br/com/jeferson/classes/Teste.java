6039f2fc18416d8eed085e8b5a958585c33e5847
jeferson <jefersonqueiroga@gmail.com>
2019-03-01 20:04:23 -0300

    Adicionei novo método de impressão.

--- AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
+++ AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
@@ -3,5 +3,10 @@ package br.com.jeferson.classes;
 public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Testando git");
+		System.out.println("Testando git");
+	}
+	
+	public void testeImpressao() {
+		System.out.println("Testando os alunos de TADS");
 	}
 }
