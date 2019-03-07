adde1fb0386c13ea696f97cc328f1a94a7a64f17
jeferson <jefersonqueiroga@gmail.com>
2019-03-07 17:44:28 -0300

    Testaqndo

--- AulaSextaCarnaval/src/br/com/jeferson/classes/Testando.java
+++ AulaSextaCarnaval/src/br/com/jeferson/classes/Testando.java
@@ -1,16 +1,22 @@
 package br.com.jeferson.classes;
 
-
 public class Testando {
-	
+
 	public void metodoLocal() {
+
+	}
+
+	public void teste() {
+		// implementar
+	}
+	
+	public void novaFuncaoPrograma() {
 		
 	}
-   
- 
-  public void teste(){
-      //implementar
-  }
+
+   	
+	public void teste2(){
+	
+	}	
 
 }
-
