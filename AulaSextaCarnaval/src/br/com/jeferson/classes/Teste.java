8cd635585cfe2882ae3afd993ca1d82abd0f7894
jeferson <jefersonqueiroga@gmail.com>
2019-03-07 20:33:02 -0300

    Correções dos commits.

--- AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
+++ AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
@@ -6,7 +6,8 @@ package br.com.jeferson.classes;
  * @author Aluno Jeferson
  * @autor Professor Michel Fernandes
  * @author Professor Alan.
- *
+ * @autor Bonfim
+ * @autor Roberto Paiva
  */
 public class Teste {
 	public static void main(String[] args) {
