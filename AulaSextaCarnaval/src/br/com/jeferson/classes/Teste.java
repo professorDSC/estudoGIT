2ed9616527477880b6454cb40da03218ef57e7dd
jeferson <jefersonqueiroga@gmail.com>
2019-03-07 19:58:11 -0300

    Corrigindo commit.

--- AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
+++ AulaSextaCarnaval/src/br/com/jeferson/classes/Teste.java
@@ -2,7 +2,8 @@ package br.com.jeferson.classes;
 
 /**
  * 
- * @author Professor jeferson Queiroga
+ * @author Professor jeferson Queiroga Pereira.
+ * @author Jeferson (aluno)
  *
  */
 public class Teste {
