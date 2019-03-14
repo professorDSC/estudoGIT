878bed72675cb131b53e9c0ccfbb02f3b553420b
jeferson <jefersonqueiroga@gmail.com>
2019-03-14 19:34:34 -0300

    Adicionar conflito

--- AulaSextaCarnaval/src/br/com/jeferson/classes/Teste2.java
+++ AulaSextaCarnaval/src/br/com/jeferson/classes/Teste2.java
@@ -4,9 +4,19 @@ public class Teste2 {
 	public void funcao1() {
 		
 	}
+	public void funcao5(){}
 	
 	public void funcao2() {
 		
 	}
 	
+		
+	
+	public void funcao3() {
+		
+	}
+	
+	public void funcao4() {
+		
+	}
 }
